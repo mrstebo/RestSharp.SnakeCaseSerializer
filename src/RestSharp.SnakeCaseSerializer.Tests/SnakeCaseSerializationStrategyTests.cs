3d92adbeb93a8ea1dbfd4af2c44e799fe80b4375
Steven Atkinson <mrstebouk@gmail.com>
2017-02-25 09:43:01 +0000

    Added the SnakeCaseSerializationStrategy and tests.

---
+++ src/RestSharp.SnakeCaseSerializer.Tests/SnakeCaseSerializationStrategyTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace RestSharp.SnakeCaseSerializer.Tests
+{
+    [TestFixture]
+    public class SnakeCaseSerializationStrategyTests
+    {
+        public SnakeCaseSerializationStrategyTests()
+        {
+            SimpleJson.CurrentJsonSerializerStrategy= new SnakeCaseSerializationStrategy();
+        }
+       
+        [Test]
+        public void Should_SerializeProperties_As_SnakeCase()
+        {
+            var json = SimpleJson.SerializeObject(new
+            {
+                Title = "Sir",
+                FirstName = "Digby",
+                LastName = "Chicken Ceasar"
+            });
+            const string expected =
+                "{\"title\":\"Sir\",\"first_name\":\"Digby\",\"last_name\":\"Chicken Ceasar\"}";
+
+            Assert.AreEqual(expected, json);
+        }
+
+        [Test]
+        public void Should_HandleNumbers_In_PropertyName()
+        {
+            var json = SimpleJson.SerializeObject(new
+            {
+                Number1Property = "test"
+            });
+            const string expected = "{\"number_1_property\":\"test\"}";
+
+            Assert.AreEqual(expected, json);
+        }
+    }
+}
---
+++ src/RestSharp.SnakeCaseSerializer/SnakeCaseSerializationStrategy.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace RestSharp.SnakeCaseSerializer
+{
+    public class SnakeCaseSerializationStrategy : PocoJsonSerializerStrategy
+    {
+        protected override string MapClrMemberNameToJsonFieldName(string clrPropertyName)
+        {
+            var sb = new StringBuilder();
+
+            for(var i = 0; i < clrPropertyName.Length; i++)
+            {
+                var c = clrPropertyName[i];
+                var pc = i > 0 ? clrPropertyName[i - 1] : '\0';
+
+                if (i > 0 && ShouldPrefixUnderscore(c, pc))
+                    sb.AppendFormat("_{0}", char.ToLower(c));
+                else
+                    sb.Append(char.ToLower(c));
+            }
+            
+            return sb.ToString();
+        }
+
+        private static bool ShouldPrefixUnderscore(char c, char pc)
+        {
+            if (pc == '_')
+                return false;
+            if (char.IsNumber(c) && !char.IsNumber(pc))
+                return true;
+            return char.IsUpper(c);
+        }
+    }
+}
